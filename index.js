22b26711960f2fb45e29e71a41e46749939b9f40
Ador Shahriar <ador.shahriar@gmail.com>
2023-10-17 19:22:58 +0600

    basic server side prepared and connection tested

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res)=> {
+    res.send('server connected')
+})
+
+app.listen(port, ()=>{
+    console.log(`connected to port ${port}`)
+})
